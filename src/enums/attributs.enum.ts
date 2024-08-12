d001c33466c851df0472eda538839a8e647c31f8
Rafael Rodrigues <rafaeltuccio@gmail.com>
2024-08-12 18:37:51 -0300

    create knightDto

---
+++ src/enums/attributs.enum.ts
@@ -0,0 +1,8 @@
+export enum AttributeKey {
+    STRENGTH = 'strength',
+    DEXTERITY = 'dexterity',
+    CONSTITUTION = 'constitution',
+    INTELLIGENCE = 'intelligence',
+    WISDOM = 'wisdom',
+    CHARISMA = 'charisma',
+  }
